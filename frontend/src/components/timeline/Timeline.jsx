b6404572e4110bb01b3b77d3f41298cd8f43f9a0
yamma10 <yamtt1326@gmail.com>
2023-01-25 22:41:05 +0900

    ページネーションの追加

--- frontend/src/components/timeline/Timeline.jsx
+++ frontend/src/components/timeline/Timeline.jsx
@@ -4,12 +4,18 @@ import Share from '../share/Share';
 import "./Timeline.css";
 import axios from "axios"
 import { AuthContext } from '../../state/AuthContext';
+import Pagination from "react-paginate";
 
 export default function Timeline({ username }) {
   const [posts, setPosts] = useState([]);
+  //現在のページ
+  const [currentPage, setCurrentPage] = useState(1);
+  //1ページあたりに表示される投稿数
+  const [postsPerPage] = useState(5);
 
   const { user } = useContext(AuthContext)
   
+  //投稿をとってくる
   useEffect(() => {
     const fetchPosts = async () => {
       const response = username 
@@ -25,6 +31,19 @@ export default function Timeline({ username }) {
     fetchPosts();
   }, [username, user._id]);
 
+  const handlePageChange = (e) => {
+    setCurrentPage(e.selected + 1);
+  }
+
+  //現在のページに表示する投稿を取得する
+
+  //現在のページの最後の投稿のインデックス
+  const indexOfLastPost = currentPage * postsPerPage;
+  //現在のページの最初の投稿のインデックス
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  //現在のページに表示する投稿の配列
+  const currentPosts = posts.slice(indexOfFirstPost,indexOfLastPost);
+
   return (
     <div className="timeline">
       <div className="timelineWrapper">
@@ -33,10 +52,23 @@ export default function Timeline({ username }) {
         : ""
         }
         
-        {posts.map((post) => (
+        {currentPosts.map((post) => (
           <Post post={post} key={post._id} />))
         }
       </div>
+      <Pagination
+        previousLabel={"前のページ"}
+        nextLabel={"次のページ"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={Math.ceil(posts.length/postsPerPage)}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageChange}
+        containerClassName={"pagination"}
+        subContainerClassName={"pages pagination"}
+        activeClassName={"active"}
+      />
     </div>
   )
 }
