d03b5e24b906be7e0ee4b74f9a4a7af71b86554d
yamma10 <yamtt1326@gmail.com>
2023-01-25 19:41:22 +0900

    backendでのファイルアップロードのsettings

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ const app = express();
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
 const postRoute = require("./routes/posts")
+const uploadRoute = require("./routes/upload");
 const PORT = 5000;
 const mongoose = require("mongoose");
+const path = require("path");
 require("dotenv").config();
 
 //Socket.io
@@ -46,10 +48,14 @@ mongoose.connect(process.env.MONGOURL).then(() => {
 
 //ミドルウェア
 app.use(express.json());
+// /imagesが指定されたとき静的ファイルの場合public/imagesまで見に行く
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 //  第1引数をルートとして設定する
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
-app.use("/api/posts", postRoute)
+app.use("/api/posts", postRoute);
+app.use("/api/upload", uploadRoute);
+
 
 
 server.listen(PORT, () => console.log("サーバーが起動しました"))
